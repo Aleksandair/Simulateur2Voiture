aecc99c55c76e1fff8fc7a4742884b9592cb346c
Popsitune <yannyavo@gmail.com>
2015-03-27 09:36:57 +0100

    Create classe testRoute + add constructor and method in IHMRoute

--- src/main/java/AppliSimu/gui/IHMRoute.java
+++ src/main/java/AppliSimu/gui/IHMRoute.java
@@ -6,4 +6,13 @@ import javax.swing.*;
  * Created by Skander on 27/03/2015.
  */
 public class IHMRoute extends JPanel {
+
+	public IHMRoute(int posX, int posY, int larg) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getlarg() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
